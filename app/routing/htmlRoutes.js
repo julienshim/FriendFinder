4eacfbb87c667631849958768578e0997302db70
jerauld <25723428+jerauld@users.noreply.github.com>
2018-06-03 01:47:30 -0700

    added html routing

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,15 @@
+var path = require("path");
+
+module.exports = function(app) {
+    
+    //A GET Route to `/survey` which should display the survey page.
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname, "/../public/survey.html"));
+    });
+
+    // Create a default, catch-all route that leaders to `home.html` which displays the home page.
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "/../public/home.html"));
+    });
+    
+}
